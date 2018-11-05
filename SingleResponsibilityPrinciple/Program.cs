2758ecd1f0646258df9121fbcd0729e930936c2f
Jonny Senger <jsenger1@css.edu>
2018-11-05 16:00:24 -0600

    Request 407 - created URLTradeDataProvider which allows you to read url data for trades.

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -2,11 +2,14 @@
 using System.Reflection;
 
 using SingleResponsibilityPrinciple.AdoNet;
+using SingleResponsibilityPrinciple.Contracts;
 
 namespace SingleResponsibilityPrinciple
 {
     class Program
     {
+        public static ITradeDataProvider UrltradeDataProvider { get; private set; }
+
         static void Main(string[] args)
         {
             var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
@@ -14,11 +17,14 @@ namespace SingleResponsibilityPrinciple
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
             var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var URlTradeDataProvider = new URLTradeDataProvider("http://faculty.css.edu/tgibbons/trades4.txt");
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
 
             var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            tradeProcessor.ProcessTrades();
+            var urltradeProcessor = new TradeProcessor(UrltradeDataProvider, tradeParser, tradeStorage);
             tradeProcessor.ProcessTrades();
 
             Console.ReadKey();
